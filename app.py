859b8e9559d5da9f260efe225d687a2104297304
Kuldeep Bhakar <223102799+kuldeep804@users.noreply.github.com>
2026-08-15 18:59:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st 
+import pandas as pd 
+import numpy as np
+
+st.title("Hello Kuldeep")
+
+st.write("This is a simple Streamlit app that demonstrates how to use Streamlit for building interactive web applications with Python.")
